a7fd354de7eae50e2b932fa7286cba2d5a100a46
kikornilov <52114814+kikornilov@users.noreply.github.com>
2025-07-20 23:47:07 +0300

    Initial commit

---
+++ src/DayOne.java
@@ -0,0 +1,9 @@
+public class DayOne {
+    public static void main(String[] args) {
+        int i = 0;
+        while (i < 10){
+            System.out.print("JAVA ");
+            i++;
+        }
+    }
+}
