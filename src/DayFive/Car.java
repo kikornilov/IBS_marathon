7c156395e74e14e691e6f592be25d04dfa0b8a69
kikornilov <52114814+kikornilov@users.noreply.github.com>
2025-07-22 22:13:23 +0300

    Done task 5.1

--- src/DayFive/Car.java
+++ src/DayFive/Car.java
@@ -4,5 +4,22 @@ public class Car {
     int year;
     String color;
     String model;
-
+    void setYear(int i){
+        year = i;
+    }
+    void setColor(String c){
+        color = c;
+    }
+    void setModel(String m){
+        model = m;
+    }
+    void getYear(){
+        System.out.println(year);
+    }
+    void getColor(){
+        System.out.println(color);
+    }
+    void getModel(){
+        System.out.println(model);
+    }
 }
