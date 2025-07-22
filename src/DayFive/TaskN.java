3cd40ec6004867cac2f426545f8c356773447adb
kikornilov <52114814+kikornilov@users.noreply.github.com>
2025-07-22 22:13:37 +0300

    Done task 5.1

---
+++ src/DayFive/TaskN.java
@@ -0,0 +1,13 @@
+package DayFive;
+
+public class TaskN {
+    public static void main(String[] args) {
+        Car c = new Car();
+        c.setColor("Yellow");
+        c.setModel("R8");
+        c.setYear(2008);
+        c.getColor();
+        c.getModel();
+        c.getYear();
+    }
+}
